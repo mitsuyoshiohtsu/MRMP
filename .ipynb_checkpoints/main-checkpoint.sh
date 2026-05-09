1fe3f6b080e081312a5894aee6f9f8dbf241dc59
mitsuyoshiohtsu <mitsuyoshiohtsu0218@gmail.com>
2026-05-09 08:32:06 +0000

    Add .gitignore for cache and checkpoint files

--- .ipynb_checkpoints/main-checkpoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python MRMP.py --dataset cifar10 --noise_type symmetric --noise_rate 0.20
--- .ipynb_checkpoints/setup-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -m pip install --upgrade pip
-pip install -q tqdm scikit-learn ema_pytorch timm
